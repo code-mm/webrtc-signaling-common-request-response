9196af6314643c4575a044c70cfd67b71275e702
m_maohuawei <maohuawei@bdlbsc.com>
2020-05-28 18:27:10 +0800

    添加Event标识

--- src/main/java/org/ms/webrtc/common/Event.java
+++ src/main/java/org/ms/webrtc/common/Event.java
@@ -22,4 +22,67 @@ public class Event<T> {
     private String event_id;
     // 事件数据
     private T data;
+
+    /**
+     * 连接
+     */
+    public static final String EVENT_CONNECT = "__event_connect";
+
+    /**
+     * 初始化注册信息
+     */
+    public static final String EVENT_INIT = "__event_init";
+    /**
+     * 被邀请
+     */
+    public static final String EVENT_INVITE = "__event_invite";
+    /**
+     * 取消
+     */
+    public static final String EVENT_CANCEL = "__event_calcel";
+
+    /**
+     * 响铃
+     */
+    public static final String EVENT_RING = "__event_ring";
+    /**
+     * 进入房间
+     */
+    public static final String EVENT_PEERS = "__event_peers";
+
+    /**
+     * 新人入房间
+     */
+    public static final String EVENT_NEW_PEERS = "__event_new_peers";
+    /**
+     * 拒绝接听
+     */
+
+    public static final String EVENT_REJECT = "__event_reject";
+
+    /**
+     * offer 主动邀请
+     */
+    public static final String EVENT_OFFER = "__event_offer";
+    /**
+     * answer 被邀请
+     */
+
+    public static final String EVENT_ANSWER = "__event_answer";
+    /**
+     * ice-candidate
+     */
+    public static final String EVENT_ICE_CANDIDATE = "__event_ice_candidate";
+    /**
+     * 离开房间
+     */
+    public static final String EVENT_LEAVE = "__event_leave";
+    /**
+     * 切换到语音
+     */
+    public static final String EVENT_AUDIO = "__event_audio";
+    /**
+     * 意外断开
+     */
+    public static final String EVENT_DISCONNECT = "__event_disconnect";
 }
